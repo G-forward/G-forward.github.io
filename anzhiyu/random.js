5e434ef25112c285caaee6e7e3e2094765b80430
G-forward <1061214956@qq.com>
2024-01-21 10:09:34 +0800

    Site updated: 2024-01-21 10:09:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/hello-world/","2024/01/20/post1/"];function toRandomPost(){
+var posts=["2024/01/20/post1/","2024/01/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
