b3f4367b8954d2429668a22fd59a34a9fa050f09
G-forward <1061214956@qq.com>
2024-05-17 14:58:06 +0800

    Site updated: 2024-05-17 14:58:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/hello-world/","2024/05/17/Vue初学-1/","2024/05/17/Vue初学/"];function toRandomPost(){
+var posts=["2024/01/20/hello-world/","2024/05/17/Vue初学-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
