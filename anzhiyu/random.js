a51e2a86c6d0980eda0533743dd5988ee0474bbe
G-forward <1061214956@qq.com>
2025-09-29 19:03:37 +0800

    Site updated: 2025-09-29 19:03:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/08/hello-world/","2025/05/08/发发法法法法/","2025/05/08/图像识别/"];function toRandomPost(){
+var posts=["2024/01/20/hello-world/","2025/05/08/发发法法法法/","2025/05/08/图像识别/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
