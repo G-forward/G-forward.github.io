cab17a264a350e7349ed2bc75c2b40a39f82d6cc
G-forward <1061214956@qq.com>
2025-12-03 18:25:51 +0800

    Site updated: 2025-12-03 18:25:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/hello-world/","2025/05/08/发发法法法法/","2025/05/08/图像识别/","2025/09/30/多层感知机/"];function toRandomPost(){
+var posts=["2024/01/20/hello-world/","2025/05/08/发发法法法法/","2025/05/08/图像识别/","2025/09/30/多层感知机/","2025/11/12/权重衰退/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
