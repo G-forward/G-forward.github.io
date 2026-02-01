098f04b1975c12dde2a06a65e909cd335371541e
G-forward <1061214956@qq.com>
2026-02-01 15:59:31 +0800

    Site updated: 2026-02-01 15:59:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/20/hello-world/","2025/05/08/图像识别/","2025/12/07/论文汇报：Physics-Driven Spatiotemporal Modeling for AI-Generated Video Detection/","2025/12/07/论文--GenWorld：面向真实世界模拟类-AI-生成视频的检测研究/","2025/09/30/多层感知机/"];function toRandomPost(){
+var posts=["2024/01/20/hello-world/","2026/01/28/发动机健康预测/","2025/05/08/图像识别/","2025/09/30/多层感知机/","2025/12/07/论文--GenWorld：面向真实世界模拟类-AI-生成视频的检测研究/","2025/12/07/论文汇报：Physics-Driven Spatiotemporal Modeling for AI-Generated Video Detection/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
